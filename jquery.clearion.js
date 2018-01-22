d4a512ab1f8c3027630b8ca83cd0f379dba521e7
Russell Wilson <ruwilson@westjet.com>
2018-01-22 10:45:56 -0700

    Refactoring to fix tabbing issue

--- jquery.clearion.js
+++ jquery.clearion.js
@@ -1,27 +1,31 @@
 ;(function ( $ ) {
 
-
     $.fn.clearion = function(options) {
 
-        var settings = $.extend({
-            // These are the defaults.
-            // color: "#556b2f",
-            // backgroundColor: "white"
-        }, options );
-        
         return this.each(function() {
-            var $wrapper = $(this).wrap('<div class="input-wrapper" />').parent();
-            $wrapper.append('<span class="clear-input" tabindex="0" />');
-            
-            $wrapper
-                .on('focusin', function() {
-                    $(this).addClass('has-focus');
-                }).on('focusout', function() {
-                    $(this).removeClass('has-focus');
-                });
+            var $input = $(this);
+            var $wrapper = $input.wrap('<div class="input-wrapper" />').parent();
+            $wrapper.append('<span class="clear-input" tabindex="0" role="button" />');
 
-            $wrapper.on('keydown', '.clear-input', function(e) {
-                if (e.which === 32 || e.which === 13) {
+            $input.on('focus', function() {
+                $wrapper.addClass('has-focus');
+                $('.clear-input', $wrapper).show();
+            });
+
+            $('.clear-input', $wrapper).on('focus', function() {
+                $wrapper.addClass('has-focus');
+            });
+
+            $input.on('blur', function() {
+                $wrapper.removeClass('has-focus');
+            });
+            $('.clear-input', $wrapper).on('blur', function() {
+                $wrapper.removeClass('has-focus');
+                $(this).hide();
+            });
+
+            $wrapper.on('keydown click', '.clear-input', function(e) {
+                if (e.which === 32 || e.which === 13 || e.which === 1) { // space enter left-click
                     e.preventDefault();
                     $(this).prev().val('').focus();
                 }
