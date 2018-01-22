db3d957613cede9ea59d606cb28ff684639d7e30
Russell Wilson <ruwilson@westjet.com>
2018-01-22 14:43:16 -0700

    Adds demo page

---
+++ demo/js/jquery.clearion.js
@@ -0,0 +1,38 @@
+'use strict';
+
+;(function ($) {
+
+    $.fn.clearion = function (options) {
+
+        return this.each(function () {
+            var $input = $(this);
+            var $wrapper = $input.wrap('<div class="input-wrapper" />').parent();
+            $wrapper.append('<span class="clear-input" tabindex="0" role="button" />');
+
+            $input.on('focus', function () {
+                $wrapper.addClass('has-focus');
+                $('.clear-input', $wrapper).show();
+            });
+
+            $('.clear-input', $wrapper).on('focus', function () {
+                $wrapper.addClass('has-focus');
+            });
+
+            $input.on('blur', function () {
+                $wrapper.removeClass('has-focus');
+            });
+            $('.clear-input', $wrapper).on('blur', function () {
+                $wrapper.removeClass('has-focus');
+                $(this).hide();
+            });
+
+            $wrapper.on('keydown click', '.clear-input', function (e) {
+                if (e.which === 32 || e.which === 13 || e.which === 1) {
+                    // space enter left-click
+                    e.preventDefault();
+                    $(this).prev().val('').focus();
+                }
+            });
+        });
+    };
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const bs = require('browser-sync').create(); // create a browser sync instance.
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+// const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('browser-sync', ['css', 'js'],function() {
+    bs.init({
+        server: {
+            baseDir: "./demo"
+        },
+        // proxy: "localhost:8080" // makes a proxy for localhost:8080
+    });
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    // gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("demo/index.html").on('change', bs.reload);
+    gulp.watch(["*.js", '!gulpfile.js'], ['js']);
+    gulp.watch(["scss/*.s{a,c}ss"], ['css']);
+});
+
+gulp.task('css', () => {
+    bs.reload();
+    return gulp.src('./scss/**/*.*')
+        .pipe(plumber())
+        .pipe(sass.sync().on('error', sass.logError))
+        // .pipe(autoprefixer())
+        .pipe(gulp.dest('demo/css'));
+  });
+
+gulp.task('js', () => {
+    bs.reload();
+    return gulp.src(['*.js', '!_*.js', '!gulpfile.js'])
+        .pipe(plumber())
+        // .pipe(concat('concat.js'))
+        .pipe(babel({
+            "presets": [
+                "env",
+                "babel-preset-stage-3"
+            ]
+        }))
+        .pipe(gulp.dest('demo/js'));
+});
