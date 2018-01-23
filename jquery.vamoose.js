853a1c6400b5ecb61d3813760e2d87712ca899a1
Russell Wilson <ruwilson@westjet.com>
2018-01-23 09:46:42 -0700

    Refactoring in prep to unit testing

--- jquery.vamoose.js
+++ jquery.vamoose.js
@@ -1,35 +1,88 @@
-;(function ( $ ) {
-
-    $.fn.vamoose = function(options) {
-
-        return this.each(function() {
-            var $input = $(this);
-            var $wrapper = $input.wrap('<div class="input-wrapper" />').parent();
-            $wrapper.append('<span class="clear-input" tabindex="0" role="button" />');
-
-            $input.on('focus', function() {
-                $wrapper.addClass('has-focus');
-                $('.clear-input', $wrapper).show();
-            });
-
-            $('.clear-input', $wrapper).on('focus', function() {
-                $wrapper.addClass('has-focus');
-            });
-
-            $input.on('blur', function() {
-                $wrapper.removeClass('has-focus');
-            });
-            $('.clear-input', $wrapper).on('blur', function() {
-                $wrapper.removeClass('has-focus');
-                $(this).hide();
-            });
-
-            $wrapper.on('keydown click', '.clear-input', function(e) {
-                if (e.which === 32 || e.which === 13 || e.which === 1) { // space enter left-click
-                    e.preventDefault();
-                    $(this).prev().val('').focus().trigger('change');
-                }
-            });
-        });
+;( function( $ ) {
+    function Vamoose( element, options ) { // eslint-disable-line  no-unused-vars
+        this.$element = $( element );
+        this.$wrapper = this.$element.wrap( "<div class=\"input-wrapper\" />" ).parent();
+        this.init();
     }
-}( jQuery ));
+
+    Vamoose.prototype.init = function() {
+        var self = this;
+        this.$wrapper.append( "<span class=\"clear-input\" tabindex=\"0\" role=\"button\" />" );
+
+        this.$element.on( "focus", function() {
+            self.elementOnFocus();
+        } );
+
+        $( ".clear-input", this.$wrapper ).on( "focus", function() {
+            self.clearOnFocus();
+        } );
+
+        this.$element.on( "blur", function() {
+            self.elementOnBlur();
+        } );
+        $( ".clear-input", this.$wrapper ).on( "blur", function() {
+            self.clearOnBlur();
+        } );
+
+        this.$wrapper.on( "keydown click", ".clear-input", function( e ) {
+            if ( e.which === 32 || e.which === 13 || e.which === 1 ) { // space enter left-click
+                e.preventDefault();
+                self.clearInput.bind( this )();
+            }
+        } );
+    };
+
+
+    /**
+     * Handles when input is given focus
+     * @returns {void}
+     */
+    Vamoose.prototype.elementOnFocus = function() {
+        this.$wrapper.addClass( "has-focus" );
+        $( ".clear-input", this.$wrapper ).show();
+    };
+
+
+    /**
+     * Handles when input is blurred
+     * @returns {void}
+     */
+    Vamoose.prototype.elementOnBlur = function() {
+        this.$wrapper.removeClass( "has-focus" );
+    };
+
+
+    /**
+     * Handles when clear button is given focus
+     * @returns {void}
+     */
+    Vamoose.prototype.clearOnFocus = function() {
+        this.$wrapper.addClass( "has-focus" );
+    };
+
+
+    /**
+     * Handles when clear button is blurred
+     * @returns {void}
+     */
+    Vamoose.prototype.clearOnBlur = function() {
+        this.$wrapper.removeClass( "has-focus" );
+        $( ".clear-input", this.$wrapper ).hide();
+    };
+
+
+    /**
+     * Handles when clear button triggered
+     * @returns {void}
+     */
+    Vamoose.prototype.clearInput = function() {
+        $( this ).prev().val( "" ).focus().trigger( "change" );
+    };
+
+    $.fn.vamoose = function( options ) {
+        return this.each( function() {
+            $.data( this, "vamoose", new Vamoose( this, options ) );
+        } );
+    };
+
+} )( jQuery );
