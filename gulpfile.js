2d93f2ae1249042c61b92f9ceeb282ebdba5a90a
Russell Wilson <russell@romeo-whiskey.com>
2018-02-17 22:32:37 -0700

    Adds non-minified & minified output

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const plumber = require('gulp-plumber');
 // const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 const sassLint = require('gulp-sass-lint');
-// const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
 gulp.task('browser-sync', ['css', 'css:demo', 'js'],function() {
     bs.init({
@@ -27,9 +30,9 @@ gulp.task('css:demo', () => {
     return gulp.src('src/scss/demo.scss')
         .pipe(plumber())
         .pipe(sassLint())
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(sassLint.format())
-        // .pipe(autoprefixer())
+        .pipe(autoprefixer())
         .pipe(gulp.dest('demo/css'));
 });
 gulp.task('css', () => {
@@ -37,9 +40,12 @@ gulp.task('css', () => {
     return gulp.src('src/scss/jquery.vamoose.scss')
         .pipe(plumber())
         .pipe(sassLint())
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(sassLint.format())
-        // .pipe(autoprefixer())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(cleanCSS())
+        .pipe(rename({ extname: '.min.css' }))
         .pipe(gulp.dest('dist/css'));
 });
 
@@ -54,5 +60,8 @@ gulp.task('js', () => {
                 'babel-preset-stage-3'
             ]
         }))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('dist/js'));;
 });
